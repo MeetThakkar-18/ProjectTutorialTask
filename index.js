e73bcf1c42e0ce7b210de82fb242c59be62621cb
Meet Thakkar <meet.thakkar@techholding.co>
2022-01-06 15:30:34 +0530

    refactor(index.js): some changes in console.log

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const swaggerDocument = require('./swagger.json');
 const tutorialpostRoutes = require('./routes/TutorialRoutes');
 
 dotenv.config();
-console.log('jdh');
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('DB Connected'));
 
